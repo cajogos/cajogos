04701c748788889d57d8fbdc0c7fcfcc802b23b6
Carlos Ferreira <c@rlos.rocks>
2020-10-25 15:47:13 +0000

    Improvements to autocommit script

--- autocommit.sh
+++ autocommit.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# crontab: /bin/bash <PWD>/autocommit.sh
+CURDIR=$(dirname "$0")
+cd $CURDIR
+
+git fetch origin && git reset --hard origin/main
+
 python3 ./main.py # Run the python script to generate README.md
 
 git add . && git commit -m "Automated commit: $(date)"
